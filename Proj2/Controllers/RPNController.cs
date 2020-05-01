1a3dd9e6c322c72c9c7d627187588ecebac1d543
Patryk-G <pgrzondziel@gmail.com>
2020-05-01 19:37:56 +0200

    proj2

---
+++ Proj2/Controllers/RPNController.cs
@@ -0,0 +1,179 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Proj2class
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RPNController : ControllerBase
+    {
+        string v = "valid formula";
+        string e = "error";
+        string fill = "Please, enter the formula correctly";
+        [HttpGet]
+        [Produces("application/json")]
+        [Route("api/tokens")]
+        public IActionResult Get (string formula)
+        {
+            if (string.IsNullOrEmpty(formula))
+            {
+                var data = new
+                {
+                    status = e,
+                    result = v
+                };
+                return BadRequest(data);
+            }
+            RPN r = new RPN(formula);
+            string[] infix = r.tokens(r.entry);
+            if (!r.validation(infix))
+            {
+                var data = new
+                {
+                    status = e,
+                    result = v
+                };
+                return BadRequest(data);
+            }
+            else
+            {
+                List<string> postfix = r.InfixToPostfix(infix);
+                var temp = new
+                {
+                    infix = infix,
+                    rpn = postfix
+                };
+                var data = new
+                {
+                    status = "ok",
+                    result = temp
+                };
+                return Ok(data);
+            }
+        }
+
+        [HttpGet]
+        [Produces("application/json")]
+        [Route("api/calculate")]
+        public IActionResult Get (string formula, double x=double.NaN)
+        {
+            if (Double.IsNaN (x))
+            {
+                var data = new 
+                {
+                    status = e,
+                    result = fill
+                };
+                return BadRequest(data);
+            }
+            if (string.IsNullOrEmpty(formula))
+            {
+                var data = new
+                {
+                    status = e,
+                    result = fill
+                };
+                return BadRequest(data);
+            }
+            RPN r = new RPN(formula);
+            r.variable = x;
+            string[] infix = r.tokens(r.entry);
+            if (!r.validation(infix))
+            {
+                var data = new 
+                {
+                    status = e,
+                    result = v
+                };
+                return BadRequest(data);
+            }
+            else 
+            {
+                List<string> postfix = r.InfixToPostfix(infix);
+                try
+                {
+                    var data = new
+                    {
+                        status = "ok",
+                        result = r.InfixToPostfix_calc(postfix)
+                    };
+                    return Ok(data);
+                }
+                catch (Exception)
+                {
+                    var data = new
+                    {
+                        status = e,
+                        result = "domain of definition"
+                    };
+                    return BadRequest(data);
+                }
+            };
+        }
+        /*
+        [HttpGet]
+        [Produces("application/json")]
+        [Route("api/calculate/xy")]
+        
+        public IActionResult Get (string formula, double from=double.NaN, double to=double.NaN, double n=double.NaN) 
+        {
+            if (string.IsNullOrEmpty(formula))
+            {
+                var data = new
+                {
+                    status = e,
+                    result = fill
+                };
+                return BadRequest(data);
+            }
+            if (Double.IsNaN (from))
+            {
+                var data = new 
+                {
+                    status = e,
+                    result = fill
+                };
+                return BadRequest(data);
+            }
+            if (Double.IsNaN (to))
+            {
+                var data = new 
+                {
+                    status = e,
+                    result = fill
+                };
+                return BadRequest(data);
+            }
+            if (Double.IsNaN (n))
+            {
+                var data = new 
+                {
+                    status = e,
+                    result = fill
+                };
+                return BadRequest(data);
+            }
+            {
+                RPN r = new RPN (formula);
+                string [] infix = r.tokens(r.entry);
+                if (!r.validation(infix))
+                {
+                    var data = new
+                    {
+                        status = e,
+                        result = v
+                    };
+                return BadRequest(data);
+                }
+                else
+                {
+                List<string> asp = new List<string>();
+                
+                
+                }
+            }
+        } 
+        */
+    }
+}
---
+++ Proj2/RPN.cs
@@ -0,0 +1,349 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Proj2class
+{
+    class RPN
+    {
+        private string entryPriv;
+        public string entry
+        {
+        get { return entryPriv; }
+        set { entryPriv = value; }
+        }
+        public RPN (string entryPriv)
+        {
+            entry = entryPriv;
+        }
+        private double variablePriv;
+        public double variable
+        {
+            get { return variablePriv; }
+            set { variablePriv = value; }
+        }
+        
+        public bool validation(string[] TokenList)
+        {
+            string lastToken = "";
+            int Lparat = 0, Pparat = 0;
+            if (op(TokenList.Last())) return false;
+            foreach (string a in TokenList)
+            {
+                if (!Regex.IsMatch(a, @"[a-zA-Z\d\(\)]+$") && !op(a))
+                {
+                    return false;
+                }
+                if (Regex.IsMatch(a, @"^[a-zA-Z]+$") && a != "x" && !func(a))
+                {
+                    return false;
+                }
+                if (a == "(")
+                {
+                    Lparat++;
+                }
+                if (a == ")")
+                {
+                    Pparat++;
+                }
+                if (a == ")" && Lparat < Pparat)
+                {
+                    return false;
+                }
+                if (op(a) && op(lastToken))
+                {
+                    return false;
+                }
+                if (Regex.IsMatch(a, @"\d+$") && lastToken != "" && lastToken != "(" && !func(lastToken) && !op(lastToken))
+                {
+                    return false;
+                }
+                if (a == "0" && lastToken == "/")
+                {
+                    return false;
+                }
+
+                lastToken = a;
+            }
+            return true;
+        }
+        static bool number(string a)
+        {
+            if (Regex.IsMatch(a, @"\d+|[x]+$"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static bool op(string a)
+        {
+            if (a == "+" || a == "-" || a == "/" || a == "*" || a == "^")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static bool func(string a)
+        {
+            if ((a == "abs" || a == "exp" || a == "log" || a == "sqrt" || a == "sin" || a == "sinh" || a == "asin" || a == "cos" || a == "cosh" || a == "acos" || a == "tan" || a == "tanh" || a == "atan") || (a == "-abs" || a == "-exp" || a == "-log" || a == "-sqrt" || a == "-sin" || a == "-sinh" || a == "-asin" || a == "-cos" || a == "-cosh" || a == "-acos" || a == "-tan" || a == "-tanh" || a == "-atan"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static double PrimaryCalc(double firs, double second, string oper)
+        {
+            if (oper == "+")
+            {
+                return firs + second;
+            }
+            if (oper == "-")
+            {
+                return second - firs;
+            }
+            if (oper == "*")
+            {
+                return firs * second;
+            }
+            if (oper == "/")
+            {
+                return second / firs;
+            }
+            if (oper == "^")
+            {
+                return Math.Pow(second, firs);
+            }
+            throw new Exception();
+        }
+
+        static double MathFunc(string func, double val)
+        {
+            if (func == "abs")
+            {
+                return Math.Abs(val);
+            }
+            if (func == "exp")
+            {
+                return Math.Exp(val);
+            }
+            if (func == "log")
+            {
+                return Math.Log(val);
+            }
+            if (func == "sqrt")
+            {
+                return Math.Sqrt(val);
+            }
+            if (func == "sin")
+            {
+                return Math.Sin(val);
+            }
+            if (func == "cos")
+            {
+                return Math.Cos(val);
+            }
+            if (func == "tan")
+            {
+                return Math.Tan(val);
+            }
+            if (func == "sinh")
+            {
+                return Math.Sinh(val);
+            }
+            if (func == "cosh")
+            {
+                return Math.Cosh(val);
+            }
+            if (func == "tanh")
+            {
+                return Math.Tanh(val);
+            }
+            if (func == "asin")
+            {
+                return Math.Asin(val);
+            }
+            if (func == "acos")
+            {
+                return Math.Acos(val);
+            }
+            if (func == "atan")
+            {
+                return Math.Atan(val);
+            }
+
+            if (func == "-abs")
+            {
+                return -Math.Abs(val);
+            }
+            if (func == "-exp")
+            {
+                return -Math.Exp(val);
+            }
+            if (func == "-log")
+            {
+                return -Math.Log(val);
+            }
+            if (func == "-sqrt")
+            {
+                return -Math.Sqrt(val);
+            }
+            if (func == "-sin")
+            {
+                return -Math.Sin(val);
+            }
+            if (func == "-cos")
+            {
+                return -Math.Cos(val);
+            }
+            if (func == "-tan")
+            {
+                return -Math.Tan(val);
+            }
+            if (func == "-sinh")
+            {
+                return -Math.Sinh(val);
+            }
+            if (func == "-cosh")
+            {
+                return -Math.Cosh(val);
+            }
+            if (func == "-tanh")
+            {
+                return -Math.Tanh(val);
+            }
+            if (func == "-asin")
+            {
+                return -Math.Asin(val);
+            }
+            if (func == "-acos")
+            {
+                return -Math.Acos(val);
+            }
+            if (func == "-atan")
+            {
+                return -Math.Atan(val);
+            }
+            throw new Exception();
+        }
+    
+        public string[] tokens(string entryPriv)
+        {
+            string buffer = entryPriv;
+            buffer = Regex.Replace(buffer, @"(?<sym>((pi|e|x|[)]|\d+(\.\d+)?)))\s+MINUS", "${sym} -");
+            buffer = Regex.Replace(buffer, @"(?<op>[+\-*/^()])", " ${op} ");
+            buffer = Regex.Replace(buffer, @"(?<function>(pi|e|sqrt|abs|exp|sqrt|log|asin|sinh|sin|cosh|acos|cos|atan|tanh|tan))", " ${function} ");
+            buffer = Regex.Replace(buffer, @"\s+", " ").Trim();
+            buffer = Regex.Replace(buffer, @"(?<number>\d+(\.\d+)?)", " ${number} ");
+            buffer = Regex.Replace(buffer, "-", "MINUS");
+            buffer = Regex.Replace(buffer, @"MINUS\s+(?<sym>((sqrt|abs|exp|sqrt|log|asin|sinh|sin|cosh|acos|cos|atan|tanh|tan|[)]|\d+(\.\d+)?)))", "-${sym}");
+            string[] Tokenlist = buffer.Split(" ".ToCharArray());
+            return Tokenlist;
+        }
+        public List<string> InfixToPostfix(string[] TokenList)
+        {
+            Stack<string> s = new Stack<string>();
+            Queue<string> q = new Queue<string>();
+            foreach (string a in TokenList)
+            {
+                if (a == "(")
+                {
+                    s.Push(a);
+                }
+                else if (a == ")")
+                {
+                    while (s.Peek() != "(")
+                    {
+                        q.Enqueue(s.Pop());
+                    }
+                    s.Pop();
+                }
+                else if (op(a) || func(a))
+                {
+                    while (s.Count > 0 && Prio(a) <= Prio(s.Peek()))
+                    {
+                        q.Enqueue(s.Pop());
+                    }
+                    s.Push(a);
+                }
+                if (number(a))
+                {
+                    q.Enqueue(a);
+                }
+            }
+            while (s.Count > 0)
+            {
+                q.Enqueue(s.Pop());
+            }
+            var list = q.ToList();
+            return list;
+        }
+
+        public double InfixToPostfix_calc(List<string> postfix)
+        {
+            Stack<string> s = new Stack<string>();
+            foreach (string p in postfix)
+            {
+                if (number(p))
+                {
+                    if (p == "x") s.Push(variable.ToString());
+                    else if (p == "-x") s.Push((variable * -1).ToString());
+                    else s.Push(p);
+                }
+                else if (func(p))
+                {
+                    double val;
+                    val = (System.Convert.ToDouble(s.Pop()));
+                    double summary = MathFunc(p, val);
+                    if (!number(summary.ToString())) throw new Exception("domain of definition");
+                    else s.Push(summary.ToString());
+                }
+                else if (op(p))
+                {
+                    double a, b;
+                    a = System.Convert.ToDouble(s.Pop(), System.Globalization.CultureInfo.InvariantCulture);
+                    b = System.Convert.ToDouble(s.Pop(), System.Globalization.CultureInfo.InvariantCulture);
+                    s.Push(PrimaryCalc(a, b, p).ToString());
+                }
+            }
+            double result;
+            result = System.Convert.ToDouble(s.Pop());
+            return result;
+        }
+        public double[,] compartments(List<string> postfix, double from, double to, int quan)
+        {
+            double[,] results = new double[2, quan];
+            double add = (to - from) / (quan - 1);
+            variablePriv = from;
+            for (int i = 0; i < quan; i++)
+            {
+                results[0, i] = variablePriv;
+                results[1, i] = InfixToPostfix_calc(postfix);
+                variablePriv += add;
+            }
+            return results;
+        }
+
+        public int Prio(string a)
+        {
+            if (func(a))
+            { return 4;}
+            else if (a == "^") {return 3;}
+            else if (a == "*" || a == "/") {return 2;}
+            else if (a == "+" || a == "-") {return 1;}
+            else {return 0;}
+        }
+    }
+}
